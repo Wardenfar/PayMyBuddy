b504f3c1c46662ad6843b79376132a56eda2dc83
Wardenfar <theo.emeriau@gmail.com>
2021-06-21 15:37:13 +0200

    [Feature] format date (thymeleaf)

---
+++ src/main/java/com/wardenfar/paymybuddy/config/ThymeleafConfig.java
@@ -0,0 +1,20 @@
+package com.wardenfar.paymybuddy.config;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    public Java8TimeDialect java8TimeDialect() {
+        return new Java8TimeDialect();
+    }
+
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
+}
